4c6857715cc6b1d1fb206bf7a1ef943374ddd309
Zyabridos <zyabrina95@gmail.com>
2023-08-30 20:07:43 +0200

    progression changed acc to pedantic requirements

--- games/progressionCheck.js
+++ games/progressionCheck.js
@@ -13,8 +13,9 @@ function isProgressionCorrect() {
 
   console.log(`'Question: ${progressionArray[0]} ${progressionArray[1]} ${progressionArray[2]} ${progressionArray[3]} ${progressionArray[4]} ${progressionArray[5]} ${progressionArray[6]} ${progressionArray[7]} ${progressionArray[8]} ${progressionArray[9]}'`);
   const userAnswer = readlineSync.question('Your answer: ');
+  if (isNaN(userAnswer) === true) console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${hiddenNumber}'.`);
   if (parseInt(userAnswer) === hiddenNumber) return true;
-  console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${hiddenNumber}'.`);
+  if (parseInt(userAnswer) !== hiddenNumber && isNaN(userAnswer) === false) console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${hiddenNumber}'.`);
 }
 
 export default isProgressionCorrect;

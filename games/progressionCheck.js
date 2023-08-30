9dd5b20b01621b76462ec14ad68d8175701e041a
Zyabridos <zyabrina95@gmail.com>
2023-08-30 18:04:36 +0200

    bug fixes

--- games/progressionCheck.js
+++ games/progressionCheck.js
@@ -7,11 +7,11 @@ function isProgressionCorrect() {
   for (let i = 0; i <= 10; i += 1) {
     progressionArray.push(firstRandomNumber + i * stepOfProgression);
   }
-  const indexOfhiddenNumber = Math.floor(Math.random() * 9) + 1;
+  const indexOfhiddenNumber = Math.floor(Math.random() * 10);
   const hiddenNumber = progressionArray[indexOfhiddenNumber];
   progressionArray[indexOfhiddenNumber] = ' .. ';
 
-  console.log(`Question: ${progressionArray[0]} ${progressionArray[1]} ${progressionArray[2]} ${progressionArray[3]} ${progressionArray[4]} ${progressionArray[5]} ${progressionArray[6]} ${progressionArray[7]} ${progressionArray[8]} ${progressionArray[9]}`);
+  console.log(`'Question: ${progressionArray[0]} ${progressionArray[1]} ${progressionArray[2]} ${progressionArray[3]} ${progressionArray[4]} ${progressionArray[5]} ${progressionArray[6]} ${progressionArray[7]} ${progressionArray[8]} ${progressionArray[9]}'`);
   const userAnswer = readlineSync.question('Your answer: ');
   if (parseInt(userAnswer) === hiddenNumber) return true;
   console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${hiddenNumber}'.`);

48bdb734ed205029a575b41ea8a4f2833cb34126
Zyabridos <zyabrina95@gmail.com>
2023-09-05 17:46:17 +0200

    bug fixes

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,10 @@
 import readlineSync from 'readline-sync';
 
-function getName() {
+function makeWelcome() {
+  console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 }
 
-export default getName;
+export default makeWelcome;
 export const userName = 'userName';

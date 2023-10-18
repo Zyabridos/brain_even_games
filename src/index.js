b480ca8e9e18c62984c8d6a36525e250058c68ed
Zyabridos <zyabrina95@gmail.com>
2023-10-18 17:17:54 +0200

    hexTest 6

--- src/index.js
+++ src/index.js
@@ -85,7 +85,7 @@ function runProgressionGame() {
     const userAnswer = readlineSync.question('Your answer: ');
     if (answer === parseInt(userAnswer, 10) && i < roundsCount - 1) {
       console.log('Correct!');
-    } else if (answer !== parseInt(userAnswer, 10)) {
+    } else if (answer !== userAnswer) {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.\nLet's try again, ${userName}!`);
       return;
     }
